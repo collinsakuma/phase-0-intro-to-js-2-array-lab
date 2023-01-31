e211ea250bdb8d4803eeddef13b3bf9c6ac65cfd
Collin Sakuma <collinmsakuma@gmail.com>
2023-01-31 08:57:30 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield',];
+
+const destructivelyAppendCat = name => {
+    cats.push(name);
+}
+
+const destructivelyPrependCat = name => {
+    cats.unshift(name);
+}
+
+const destructivelyRemoveLastCat = name => {
+    cats.pop(name);
+}
+
+const destructivelyRemoveFirstCat = name => {
+    cats.shift(name);
+}
+
+const appendCat = name => {
+   return [...cats, name];
+}
+
+const prependCat = name => {
+    return [name, ... cats];
+}
+
+const removeLastCat = () => {
+    return [...cats.slice(0, cats.length -1)];
+}
+
+const removeFirstCat = () => {
+    return [...cats.slice(1)];
+}
